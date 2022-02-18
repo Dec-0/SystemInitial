b02ab990300ca74b77ff937ce1c50e516a7af8ec
XieZD <36985558+Dec-0@users.noreply.github.com>
2022-02-18 14:20:05 +0800

    Add files via upload

---
+++ DB.sh
@@ -0,0 +1,173 @@
+# This script for ordinary database files
+# Executable files and related
+Perl="/biobiggen/data/headQuarter/user/xiezhangdong/Softs/perl-5.26.3/bin/perl"
+Dir4Annovar="/biobiggen/data/headQuarter/user/xiezhangdong/Scripts/Annotation/Annovar"
+Ascp="/biobiggen/data/headQuarter/user/xiezhangdong/Softs/aspera-cli-3.9.6/cli/bin/ascp"
+Key410KGP="/biobiggen/data/headQuarter/user/xiezhangdong/Softs/aspera-cli-3.9.6/cli/etc/asperaweb_id_dsa.openssh"
+# Other
+Dir4Work=$(pwd)
+Flag4RefHg38=false
+Flag4LiftOverChain=false
+Flag4NMAndAA=false
+Flag4Annotation=false
+# MAF
+Flag410KGP=false
+Flag4dbsnp=false
+Flag4gnomAD=false
+
+# Reference ucsc.hg38.fasta
+if ${Flag4RefHg38} ; then
+	if [ -s ${Dir4Work}/Reference/hg38/ucsc.hg38.fasta ] ; then
+		echo "File exit (${Dir4Work}/Reference/hg38/ucsc.hg38.fasta)"
+	else
+		[ ! -d ${Dir4Work}/Reference/hg38/tmpDir4Hg38 ] && mkdir -p ${Dir4Work}/Reference/hg38/tmpDir4Hg38
+		
+		cd ${Dir4Work}/Reference/hg38/tmpDir4Hg38
+		Midfix=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "X" "Y")
+		for z in ${Midfix[@]}
+		do
+			wget "ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/chr${z}.fa.gz" -O chr${z}.fa.gz
+		done
+		
+		gunzip chr*.fa.gz
+		cat chr1.fa chr2.fa chr3.fa chr4.fa chr5.fa \
+			chr6.fa chr7.fa chr8.fa chr9.fa chr10.fa \
+			chr11.fa chr12.fa chr13.fa chr14.fa chr15.fa \
+			chr16.fa chr17.fa chr18.fa chr19.fa chr20.fa \
+			chr21.fa chr22.fa chrX.fa chrY.fa > ucsc.hg38.fasta
+		mv ucsc.hg38.fasta ${Dir4Work}/Reference/hg38
+		
+		cd ${Dir4Work}/Reference/hg38
+		rm -r ${Dir4Work}/Reference/hg38/tmpDir4Hg38
+		[ -s ${Dir4Work}/Reference/hg38/ucsc.hg38.fasta ] && echo '[ Info ] Done for reference fasta of hg38'
+	fi
+fi
+
+# liftover chain file
+if ${Flag4LiftOverChain} ; then
+	if [ -s ${Dir4Work}/ChainOfLiftOver/hg19ToHg38.over.chain.gz && -s ${Dir4Work}/ChainOfLiftOver/hg38ToHg19.over.chain.gz ] ; then
+		echo "File exit (${Dir4Work}/ChainOfLiftOver/hg38/hg19ToHg38.over.chain.gz and ${Dir4Work}/ChainOfLiftOver/hg38/hg38ToHg19.over.chain.gz)"
+	else
+		[ ! -d ${Dir4Work}/ChainOfLiftOver ] && mkdir -p ${Dir4Work}/ChainOfLiftOver
+		
+		cd ${Dir4Work}/ChainOfLiftOver
+		wget 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/liftOver/hg38ToHg19.over.chain.gz' -O hg38ToHg19.over.chain.gz
+		wget 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz' -O hg19ToHg38.over.chain.gz
+		echo '[ Info ] Done for liftover chain files'
+	fi
+fi
+
+# nucleotides and amino acid
+if ${Flag4NMAndAA} ; then
+	if [ -s ${Dir4Work}/NMAndAA/hg38/refGene.txt.gz && -s ${Dir4Work}/NMAndAA/hg38/cytoBand.txt.gz ] ; then
+		echo "File exit (${Dir4Work}/NMAndAA/hg38/refGene.txt.gz and ${Dir4Work}/NMAndAA/hg38/cytoBand.txt.gz)"
+	else
+		[ ! -d ${Dir4Work}/NMAndAA/hg38 ] && mkdir -p ${Dir4Work}/NMAndAA/hg38
+		
+		cd ${Dir4Work}/NMAndAA/hg38
+		wget 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/cytoBand.txt.gz' -O cytoBand.txt.gz
+		wget 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/refGene.txt.gz' -O refGene.txt.gz
+		echo '[ Info ] Done for files of nucleotides and amino acids'
+	fi
+fi
+
+# Annotation
+if ${Flag4Annotation} ; then
+	if [ -s ${Dir4Work}/Annotation/hg38/hg38_refGeneMrna.fa ] ; then
+		echo "File exit (${Dir4Work}/Annotation/hg38/hg38_refGeneMrna.fa)"
+	else
+		[ ! -d ${Dir4Work}/Annotation/hg38 ] && mkdir -p ${Dir4Work}/Annotation/hg38
+		
+		cd ${Dir4Work}/Annotation/hg38
+		#### 基因注释相关
+		# ${Perl} ${Dir4Annovar}/annotate_variation.pl -buildver hg38 -downdb refGene ${Dir4Work}/Annotation/hg38
+		wget 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/refGene.txt.gz' -O hg38_refGene.txt.gz
+		gunzip -f hg38_refGene.txt.gz
+		# ${Perl} ${Dir4Annovar}/annotate_variation.pl -buildver hg38 -downdb seq ${Dir4Work}/Annotation/hg38/hg38_seq
+		[ ! -d ${Dir4Work}/Annotation/hg38/hg38_seq ] && mkdir ${Dir4Work}/Annotation/hg38/hg38_seq
+		cd ${Dir4Work}/Annotation/hg38/hg38_seq
+		wget 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.chromFa.tar.gz' -O hg38.chromFa.tar.gz
+		wget 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz' -O hg38.fa.gz
+		tar -xzf hg38.chromFa.tar.gz
+		gunzip -f hg38.fa.gz
+		${Perl} ${Dir4Annovar}/retrieve_seq_from_fasta.pl ${Dir4Work}/Annotation/hg38/hg38_refGene.txt -seqdir ${Dir4Work}/Annotation/hg38/hg38_seq/chroms -format refGene -outfile ${Dir4Work}/Annotation/hg38/hg38_refGeneMrna.fa
+		[ -d ${Dir4Work}/Annotation/hg38/hg38_seq ] && rm -r ${Dir4Work}/Annotation/hg38/hg38_seq
+		cd ${Dir4Work}/Annotation/hg38
+		
+		#### 人群频率相关
+		# ${Perl} ${Dir4Annovar}/annotate_variation.pl -buildver hg38 -downdb 1000g2015aug ${Dir4Work}/Annotation/hg38
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_1000g2015aug.zip' -O hg38_1000g2015aug.zip
+		unzip -o hg38_1000g2015aug.zip
+		[ -s ${Dir4Work}/Annotation/hg38/hg38_1000g2015aug.zip ] && rm ${Dir4Work}/Annotation/hg38/hg38_1000g2015aug.zip
+		# ${Perl} ${Dir4Annovar}/annotate_variation.pl -buildver hg38 -downdb gnomad211_exome -webfrom annovar ${Dir4Work}/Annotation/hg38
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_gnomad211_exome.txt.gz' -O hg38_gnomad211_exome.txt.gz
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_gnomad211_exome.txt.idx.gz' -O hg38_gnomad211_exome.txt.idx.gz
+		gunzip -f hg38_gnomad211_exome.txt.gz
+		gunzip -f hg38_gnomad211_exome.txt.idx.gz
+		# ${Perl} ${Dir4Annovar}/annotate_variation.pl -buildver hg38 -downdb esp6500siv2_all -webfrom annovar ${Dir4Work}/Annotation/hg38
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_esp6500siv2_all.txt.gz' -O hg38_esp6500siv2_all.txt.gz
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_esp6500siv2_all.txt.idx.gz' -O hg38_esp6500siv2_all.txt.idx.gz
+		gunzip -f hg38_esp6500siv2_all.txt.gz
+		gunzip -f hg38_esp6500siv2_all.txt.idx.gz
+		
+		#### 功能预测（cosmic、clinvar、ljb26、dbnsfp30a）
+		# ${Perl} ${Dir4Annovar}/annotate_variation.pl -buildver hg38 -downdb cosmic70 -webfrom annovar ${Dir4Work}/Annotation/hg38
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_cosmic70.txt.gz' -O hg38_cosmic70.txt.gz
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_cosmic70.txt.idx.gz' -O hg38_cosmic70.txt.idx.gz
+		gunzip -f hg38_cosmic70.txt.gz
+		gunzip -f hg38_cosmic70.txt.idx.gz
+		# ${Perl} ${Dir4Annovar}/annotate_variation.pl -buildver hg38 -downdb clinvar_20210501 -webfrom annovar ${Dir4Work}/Annotation/hg38
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_clinvar_20210501.txt.gz' -O hg38_clinvar_20210501.txt.gz
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_clinvar_20210501.txt.idx.gz' -O hg38_clinvar_20210501.txt.idx.gz
+		gunzip -f hg38_clinvar_20210501.txt.gz
+		gunzip -f hg38_clinvar_20210501.txt.idx.gz
+		# ${Perl} ${Dir4Annovar}/annotate_variation.pl -buildver hg38 -downdb ljb26_all -webfrom annovar ${Dir4Work}/Annotation/hg38
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_ljb26_all.txt.gz' -O hg38_ljb26_all.txt.gz
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_ljb26_all.txt.idx.gz' -O hg38_ljb26_all.txt.idx.gz
+		gunzip -f hg38_ljb26_all.txt.gz
+		gunzip -f hg38_ljb26_all.txt.idx.gz
+		# ${Perl} ${Dir4Annovar}/annotate_variation.pl -buildver hg38 -downdb dbnsfp30a -webfrom annovar ${Dir4Work}/Annotation/hg38
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_dbnsfp30a.txt.gz' -O hg38_dbnsfp30a.txt.gz
+		wget 'http://www.openbioinformatics.org/annovar/download/hg38_dbnsfp30a.txt.idx.gz' -O hg38_dbnsfp30a.txt.idx.gz
+		gunzip -f hg38_dbnsfp30a.txt.gz
+		gunzip -f hg38_dbnsfp30a.txt.idx.gz
+		
+		echo '[ Info ] Done for annotation files'
+	fi
+fi
+
+# MAF
+# 10KGP
+if ${Flag410KGP} && [ ! -s ${Dir4Work}/MAF/10KGP/ALL.chr1.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz ] ; then
+	[ ! -d ${Dir4Work}/MAF/10KGP ] && mkdir -p ${Dir4Work}/MAF/10KGP
+	Midfix=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "X" "Y")
+	for z in ${Midfix[@]}
+	do
+		${Ascp} -i ${Key410KGP} -Tr -Q -l 100M -P33001 -L- fasp-g1k@fasp.1000genomes.ebi.ac.uk:vol1/ftp/release/20110521/ALL.chr${z}.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz ${Dir4Work}/MAF/10KGP
+		${Ascp} -i ${Key410KGP} -Tr -Q -l 100M -P33001 -L- fasp-g1k@fasp.1000genomes.ebi.ac.uk:vol1/ftp/release/20110521/ALL.chr${z}.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz.tbi ${Dir4Work}/MAF/10KGP
+	done
+fi
+# dbsnp
+if ${Flag4dbsnp} && [ ! -s ${Dir4Work}/MAF/dbsnp/freq.GRCh38.vcf.gz ] ; then
+	[ ! -d ${Dir4Work}/MAF/dbsnp ] && mkdir -p ${Dir4Work}/MAF/dbsnp
+	
+	cd ${Dir4Work}/MAF/dbsnp
+	wget 'ftp://ftp.ncbi.nih.gov/snp/population_frequency/latest_release/freq.vcf.gz' -O freq.vcf.gz
+	wget 'ftp://ftp.ncbi.nih.gov/snp/population_frequency/latest_release/freq.vcf.gz.tbi' -O freq.vcf.gz.tbi
+	wget 'ftp://ftp-trace.ncbi.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/latest_assembly_versions/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_assembly_report.txt' -O GCF_000001405.39_GRCh38.p13_assembly_report.txt
+fi
+# gnomAD
+if ${Flag4gnomAD} && [ ! -s ${Dir4Work}/MAF/gnomAD/gnomad.exomes.r2.1.1.sites.1.vcf.bgz ] ; then
+	[ ! -d ${Dir4Work}/MAF/gnomAD ] && mkdir -p ${Dir4Work}/MAF/gnomAD
+	cd ${Dir4Work}/MAF/gnomAD
+	Midfix=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "X" "Y")
+	for z in ${Midfix[@]}
+	do
+		wget "ftp://datasetgnomad.blob.core.windows.net/dataset/release/2.1.1/vcf/exomes/gnomad.exomes.r2.1.1.sites.${z}.vcf.bgz" -O gnomad.exomes.r2.1.1.sites.${z}.vcf.bgz
+		wget "ftp://datasetgnomad.blob.core.windows.net/dataset/release/2.1.1/vcf/exomes/gnomad.exomes.r2.1.1.sites.${z}.vcf.bgz.tbi" -O gnomad.exomes.r2.1.1.sites.${z}.vcf.bgz
+	done
+fi
+
+# Ensembl
+
+# NA12878
